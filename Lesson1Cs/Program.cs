dc12bc151403d6bc81865590dd7cc4cb89cbcaa8
LaL <LaL@L-ALCATRAST>
2025-03-29 10:43:31 +0300

    Add project files.

---
+++ Lesson1Cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Lesson1Cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // int a, b;
+            bool f = int.TryParse(Console.ReadLine(), out int a);
+
+        }
+
+
+
+    }
+}
